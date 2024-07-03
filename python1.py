44d7b6a36bef4f722fe81ea128a558dfbd55b40b
Alli Sodeeq <sodeeqalli@gmail.com>
2024-07-03 21:41:51 +0100

    Initial Commit

---
+++ python1.py
@@ -0,0 +1,13 @@
+
+character_name = "Tom"
+character_age = 50.5678213
+is_male= False
+
+print ("There was once a man named " + character_name +", ")
+print (" he was " + character_name +" years old. ")
+
+character_name = "Sodeeq"
+
+print (" he really liked the name " + character_name +", ")
+print (" but didn't like being "+ character_age  +" , ")
+
